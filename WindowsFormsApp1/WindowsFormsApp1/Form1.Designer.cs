2eae1e39ade8121d27068c5e481b1c642fff410a
Dan <robotop2006@gmail.com>
2019-12-15 19:23:03 +0300

    Proyect

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.reset = new System.Windows.Forms.Button();
+            this.paperBtn = new System.Windows.Forms.Button();
+            this.scissorsBtn = new System.Windows.Forms.Button();
+            this.stoneBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.Location = new System.Drawing.Point(6, 13);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1048, 186);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // reset
+            // 
+            this.reset.BackColor = System.Drawing.Color.Transparent;
+            this.reset.BackgroundImage = global::WindowsFormsApp1.Properties.Resources._5a0c6dcf9642de34b6b65cc5;
+            this.reset.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.reset.Location = new System.Drawing.Point(276, 209);
+            this.reset.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.reset.Name = "reset";
+            this.reset.Size = new System.Drawing.Size(552, 123);
+            this.reset.TabIndex = 5;
+            this.reset.UseVisualStyleBackColor = false;
+            this.reset.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // paperBtn
+            // 
+            this.paperBtn.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.paper_sheet_PNG7234;
+            this.paperBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.paperBtn.Location = new System.Drawing.Point(731, 340);
+            this.paperBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.paperBtn.Name = "paperBtn";
+            this.paperBtn.Size = new System.Drawing.Size(333, 199);
+            this.paperBtn.TabIndex = 4;
+            this.paperBtn.UseVisualStyleBackColor = true;
+            this.paperBtn.Click += new System.EventHandler(this.PaperBtn_Click);
+            // 
+            // scissorsBtn
+            // 
+            this.scissorsBtn.BackColor = System.Drawing.Color.Transparent;
+            this.scissorsBtn.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.w256h2561339195679Cut256x256;
+            this.scissorsBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.scissorsBtn.Location = new System.Drawing.Point(371, 340);
+            this.scissorsBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.scissorsBtn.Name = "scissorsBtn";
+            this.scissorsBtn.Size = new System.Drawing.Size(333, 199);
+            this.scissorsBtn.TabIndex = 3;
+            this.scissorsBtn.UseVisualStyleBackColor = false;
+            this.scissorsBtn.Click += new System.EventHandler(this.ScissorsBtn_Click);
+            // 
+            // stoneBtn
+            // 
+            this.stoneBtn.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.stone_PNG13558;
+            this.stoneBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.stoneBtn.Location = new System.Drawing.Point(16, 340);
+            this.stoneBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.stoneBtn.Name = "stoneBtn";
+            this.stoneBtn.Size = new System.Drawing.Size(333, 199);
+            this.stoneBtn.TabIndex = 1;
+            this.stoneBtn.UseVisualStyleBackColor = true;
+            this.stoneBtn.Click += new System.EventHandler(this.StoneBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.reset);
+            this.Controls.Add(this.paperBtn);
+            this.Controls.Add(this.scissorsBtn);
+            this.Controls.Add(this.stoneBtn);
+            this.Controls.Add(this.richTextBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button stoneBtn;
+        private System.Windows.Forms.Button paperBtn;
+        private System.Windows.Forms.Button scissorsBtn;
+        private System.Windows.Forms.Button reset;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        int var;
+        Random rnd = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+            var = rnd.Next(0, 3);
+
+        }
+
+        private void StoneBtn_Click(object sender, EventArgs e)
+        {
+            check_result(0);
+        }
+
+        private void check_result(int user_result)
+        {
+            if (var == 0)
+            {
+                stoneBtn.BackColor = Color.Aqua;
+            }
+            else if (var == 1)
+            {
+                scissorsBtn.BackColor = Color.Aqua;
+            }
+            else if (var == 2)
+            {
+                paperBtn.BackColor = Color.Aqua;
+            }
+
+            if (user_result == var)
+            {
+                richTextBox1.Text = "Ничья";
+            } 
+            else if (user_result == 0 && var == 1)
+            {
+                richTextBox1.Text = "Ты выйграл!";
+            }
+            else if (user_result == 0 && var == 2)
+            {
+                richTextBox1.Text = "Ты проиграл!";
+            }
+            else if (user_result == 1 && var == 2)
+            {
+                richTextBox1.Text = "Ты выйграл!";
+            }
+            else if (user_result == 1 && var == 0)
+            {
+                richTextBox1.Text = "Ты проиграл!";
+            }
+            else if (user_result == 2 && var == 0)
+            {
+                richTextBox1.Text = "Ты выйграл!";
+            }
+            else if (user_result == 2 && var == 1)
+            {
+                richTextBox1.Text = "Ты проиграл!";
+            }
+        }
+
+        private void ScissorsBtn_Click(object sender, EventArgs e)
+        {
+            check_result(1);
+        }
+
+        private void PaperBtn_Click(object sender, EventArgs e)
+        {
+            check_result(2);
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            var = rnd.Next(0, 3);
+            stoneBtn.BackColor = Color.Transparent;
+            scissorsBtn.BackColor = Color.Transparent;
+            paperBtn.BackColor = Color.Transparent;
+            richTextBox1.Text = "";
+        }
+    }
+}
